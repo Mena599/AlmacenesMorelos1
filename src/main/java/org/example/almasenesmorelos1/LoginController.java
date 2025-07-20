f20914dc5a84dab89a0f6d8dfceb91d50f9bea4f
Mena599 <20243rd010@utez.edu.mx>
2025-07-20 17:48:47 -0600

    Modificaciones en Login+controller y HelloApplication

--- src/main/java/org/example/almasenesmorelos1/LoginController.java
+++ src/main/java/org/example/almasenesmorelos1/LoginController.java
@@ -1,4 +1,29 @@
 package org.example.almasenesmorelos1;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class LoginController {
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtContrasena;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private void handleLogin() {
+        String usuario = txtUsuario.getText().trim();
+        String contrasena = txtContrasena.getText().trim();
+
+        if (usuario.equals("root") && contrasena.equals("admin123")) {
+            lblMensaje.setText("✅ Bienvenido, SuperAdmin.");
+            // Aquí puedes cargar otra escena más adelante
+        } else {
+            lblMensaje.setText("❌ Credenciales incorrectas.");
+        }
+    }
+
 }

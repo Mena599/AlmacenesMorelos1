6dd01f892f481ed0e4876e3c7943553e689cc6f9
Mena599 <20243rd010@utez.edu.mx>
2025-07-23 20:54:18 -0600

    solo agrege un boton en iniciode super admin

--- src/main/java/org/example/almasenesmorelos1/InicioSuperAdminController.java
+++ src/main/java/org/example/almasenesmorelos1/InicioSuperAdminController.java
@@ -1,4 +1,34 @@
 package org.example.almasenesmorelos1;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class InicioSuperAdminController {
+
+    @FXML
+    private Button btnClientes;
+
+    @FXML
+    private void abrirVentana2(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Clientes.fxml"));
+            Parent root = loader.load();
+
+            // Obtener el stage actual desde el botón que se presionó
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Clientes");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

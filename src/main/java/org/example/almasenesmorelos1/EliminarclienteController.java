8066b7bbb1625e18e9b536f6ef3f23d7d95b74d9
Mena599 <20243rd010@utez.edu.mx>
2025-08-03 14:44:30 -0600

    Ahora toca aliminar cliente

---
+++ src/main/java/org/example/almasenesmorelos1/EliminarclienteController.java
@@ -0,0 +1,14 @@
+package org.example.almasenesmorelos1;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class EliminarclienteController {
+
+    @FXML private Label lblAlerta;
+    @FXML private Button bnbsi;
+    @FXML private Button btnno;
+
+
+}
--- src/main/java/org/example/almasenesmorelos1/TargetasClienteController.java
+++ src/main/java/org/example/almasenesmorelos1/TargetasClienteController.java
@@ -1,6 +1,8 @@
 package org.example.almasenesmorelos1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class TargetasClienteController {
@@ -8,6 +10,7 @@ public class TargetasClienteController {
     @FXML private Label lblNombre;
     @FXML private Label lblCorreo;
     @FXML private Label lblTelefono;
+    @FXML private Button btnEliminar;
     public void setLblNombre(String lblNombre) {
         this.lblNombre.setText(lblNombre);
     }
@@ -16,5 +19,10 @@ public class TargetasClienteController {
     }
     public void setLblTelefono(String lblTelefono) {
         this.lblTelefono.setText(lblTelefono);
+    }
+    public void setBtnEliminar(Button btnEliminar) { this .btnEliminar = btnEliminar; }
+
+    public void  OnEliminarAction(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TargetasCliente.fxml"));
     }
 }

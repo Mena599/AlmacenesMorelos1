d5edffcea7349bb10ee30290ee4c2397a379be80
Mena599 <20243rd010@utez.edu.mx>
2025-08-06 17:47:59 -0600

    Empeze la vista de inicio

--- src/main/java/org/example/almasenesmorelos1/HelloApplication.java
+++ src/main/java/org/example/almasenesmorelos1/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Clientes.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("ALmacenes Morelos");
         stage.setScene(scene);
         stage.show();

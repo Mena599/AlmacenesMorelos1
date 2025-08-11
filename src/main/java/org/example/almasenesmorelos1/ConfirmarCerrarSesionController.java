36b86de81da88c7155b94a797aaa579f3df166f3
Selegna01 <20243rd008@utez.edu.mx>
2025-08-10 21:57:54 -0600

    Se han creado los archivos de las ventanas emergentes para eliminar Sedes y cerrar sesión
    Signed-off-by: Selegna01 <20243rd008@utez.edu.mx>

---
+++ src/main/java/org/example/almasenesmorelos1/ConfirmarCerrarSesionController.java
@@ -0,0 +1,38 @@
+package org.example.almasenesmorelos1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ConfirmarCerrarSesionController {
+
+    private boolean confirmado = false;
+
+    public boolean isConfirmado() {
+        return confirmado;
+    }
+
+    @FXML
+    private Button btnsi;
+
+    @FXML
+    private Button btnno;
+
+    @FXML
+    private void onSiAction(ActionEvent event) {
+        confirmado = true;
+        cerrarVentana(event);
+    }
+
+    @FXML
+    private void onNoAction(ActionEvent event) {
+        confirmado = false;
+        cerrarVentana(event);
+    }
+
+    private void cerrarVentana(ActionEvent event) {
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+}
